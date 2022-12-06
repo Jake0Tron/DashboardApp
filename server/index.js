82078d7cb61eab8453d9deeb84d6630af5e26ddc
Jake Deacon <deaconj02@gmail.com>
2022-12-06 10:39:17 -0500

    adding subcomponenents for weather display, building out express server

--- server/index.js
+++ server/index.js
@@ -1,45 +1,98 @@
 const fetch = (...args) =>
-	import('node-fetch').then(({default: fetch}) => fetch(...args));
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const port = 8080;
+const { getTrimmedLatLon } = require("./utils");
 
-const API_KEY = '85d47c9ef88fa239046e9449dfa49367'
+const API_KEY = "85d47c9ef88fa239046e9449dfa49367";
 
 app.use(cors(), bodyParser.json());
 
 /**
- * TODO: 
+ * TODO:
  * https://openweathermap.org/forecast5
  * https://openweathermap.org/api/weathermaps
  * https://openweathermap.org/api/air-pollution
  * https://openweathermap.org/api/geocoding-api
+ *
+ * possibly cache results based on availability?
  */
 
-app.get('/weather', async (req, res) => {
-  const {lat, lon} = req.query;
-  const trimLat = parseFloat(lat).toFixed(2)
-  const trimLon = parseFloat(lon).toFixed(2)
-  console.info('weather fetching for', req.query)
-  
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${trimLat}&lon=${trimLon}&appid=${API_KEY}`
-  
-  const response = await fetch(url)
-    .catch((e) => {
-    console.error(e)
-    res.send(e)
-  })
+app.get("/weather", async (req, res) => {
+  const { lat, lon } = getTrimmedLatLon(req);
+  console.info("weather fetching for", req.query);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  const response = await fetch(url).catch((e) => {
+    console.error(e);
+    res.send(e);
+  });
 
   const data = await response.json();
 
-  res.send(data)
-})
+  res.send(data);
+});
 
-/* TODO: fetch weather data:
-    - 
-*/
+//  * https://openweathermap.org/forecast5
+app.get("/forecast", async (req, res) => {
+  const { lat, lon } = getTrimmedLatLon(req);
+
+  console.info(`forecast fetching for ${lat}, ${lon}`);
+
+  const url = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  const response = await fetch(url).catch((e) => {
+    console.error(e);
+    res.send(e);
+  });
+
+  const data = await response.json();
+
+  res.send(data);
+});
+
+// * https://openweathermap.org/api/weathermaps
+app.get("/weather-map", async (req, res) => {
+  const { lat, lon } = getTrimmedLatLon(req);
+
+  const url = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  // TODO: Look into displaying weather data via
+  // https://github.com/google/maps-for-work-samples/blob/master/samples/maps/OpenWeatherMapLayer/OpenWeatherMapLayer.pdf
+  const response = await fetch(url).catch((e) => {
+    console.error(e);
+    res.send(e);
+  });
+
+  const data = await response.json();
+
+  res.send(data);
+});
+
+// * https://openweathermap.org/api/air-pollution
+app.get("/air-pollution", async (req, res) => {
+  const { lat, lon } = getTrimmedLatLon(req);
+
+  const url = `api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  // TODO: Historical
+  // http://api.openweathermap.org/data/2.5/air_pollution/history?lat={lat}&lon={lon}&start={start}&end={end}&appid={API key}
+
+  const response = await fetch(url).catch((e) => {
+    console.error(e);
+    res.send(e);
+  });
+
+  const data = await response.json();
+
+  res.send(data);
+});
+
+// * https://openweathermap.org/api/geocoding-api
+app.get("", (req, res) => {});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import Weather from './components/Weather/Weather'
-import './App.css';
+import {
+  AirPollutionWeather,
+  CurrentWeather,
+  ForecastWeather,
+} from "./components/Weather";
+import "./App.css";
+
+/* TODO: 
+  - Set up a series of small widgets
+    - various weather things
+    - news?
+      - set keywords for interests?
+  - Drag/drop UI arrangement for widgets
+
+   Maybe look at Google Maps API or something for rendering/location selection
+    https://developers.google.com/maps/documentation/javascript
+
+  build out components for: 
+    - current weather
+      - https://openweathermap.org/current#current_JSON
+    - alerts
+    - daily?
+    - hourly? 
+
+    see https://openweathermap.org/widgets-constructor for example styling
+*/
 
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <CurrentWeather />
+      <ForecastWeather />
+      <AirPollutionWeather />
     </div>
   );
 }
