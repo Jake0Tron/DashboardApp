c7fe7b96b3079df76749139ffbbebc8459d9d758
Jake Deacon <deaconj02@gmail.com>
2022-12-15 10:50:14 -0500

    colors

--- src/components/Weather/components/Carousel/Carousel.js
+++ src/components/Weather/components/Carousel/Carousel.js
@@ -8,25 +8,25 @@ const getTilesToShow = (tiles, index, numToShow) => {
   return tiles.slice(index, index + numToShow);
 };
 
-const NUM_TILES = 2;
+const NUM_TILES = 3;
 const Carousel = ({ carouselData, tileCount = NUM_TILES }) => {
   const [tileIndex, setTileIndex] = useState(0);
   const [tiles, setTiles] = useState(
-    getTilesToShow(carouselData, tileIndex, NUM_TILES)
+    getTilesToShow(carouselData, tileIndex, tileCount)
   );
 
   const onNextClick = () => {
-    let sliceIndex = tileIndex + NUM_TILES;
-    if (sliceIndex > carouselData.length - NUM_TILES - 1)
-      sliceIndex = carouselData.length - NUM_TILES - 1;
-    setTiles(getTilesToShow(carouselData, sliceIndex, NUM_TILES));
+    let sliceIndex = tileIndex + tileCount;
+    if (sliceIndex > carouselData.length - tileCount - 1)
+      sliceIndex = carouselData.length - tileCount - 1;
+    setTiles(getTilesToShow(carouselData, sliceIndex, tileCount));
     setTileIndex(sliceIndex);
   };
 
   const onPrevClick = () => {
-    let sliceIndex = tileIndex - NUM_TILES;
+    let sliceIndex = tileIndex - tileCount;
     if (sliceIndex < 0) sliceIndex = 0;
-    setTiles(getTilesToShow(carouselData, sliceIndex, NUM_TILES));
+    setTiles(getTilesToShow(carouselData, sliceIndex, tileCount));
     setTileIndex(sliceIndex);
   };
 
@@ -47,7 +47,7 @@ const Carousel = ({ carouselData, tileCount = NUM_TILES }) => {
         </button>
         <button
           className="carouselButton"
-          disabled={tileIndex === carouselData.length - NUM_TILES - 1}
+          disabled={tileIndex === carouselData.length - tileCount - 1}
           onClick={onNextClick}
         >
           &gt;
